79566119f22f5bbcc1a0ad21c71ef472ce3f04b0
Addi Geilke <alagieaddi.geilke@cgi.com>
2024-10-07 17:08:32 +0200

    add Client

---
+++ Client/WebSocket/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using WebSocketSharp;
+
+namespace WebSocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userId = "User1";
+            using (var ws = new WebSocket("ws://<Host ip:host port>/Echo"))
+            {
+                ws.OnMessage += (sender, e) =>
+                    Console.WriteLine("Received from server: " + e.Data);
+
+                ws.Connect();
+                Console.WriteLine("Connected to ws://<Host ip:host port>/Echo");
+
+                string message;
+                while ((message = Console.ReadLine()) != null)
+                {
+                    string payload = $"{userId}: {message}";
+                    ws.Send(payload);
+                }
+            }
+        }
+    }
+}
